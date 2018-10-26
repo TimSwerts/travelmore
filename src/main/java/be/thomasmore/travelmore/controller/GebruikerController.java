24a005640b51df8647bdcf374d853fb6f121c625
robbert van hove <robbertvanhove1@gmail.com>
2018-10-26 09:36:08 +0200

    basis registratie

--- src/main/java/be/thomasmore/travelmore/controller/GebruikerController.java
+++ src/main/java/be/thomasmore/travelmore/controller/GebruikerController.java
@@ -3,12 +3,32 @@ package be.thomasmore.travelmore.controller;
 import be.thomasmore.travelmore.domain.Gebruiker;
 import be.thomasmore.travelmore.service.GebruikerService;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
+import java.io.Serializable;
+import java.util.Date;
 
-public class GebruikerController {
-    private Gebruiker gebruiker = new Gebruiker();
+
+@ManagedBean
+@ViewScoped
+public class GebruikerController implements Serializable  {
+    private Gebruiker newGebruiker = new Gebruiker();
+
 
     @Inject
     private GebruikerService gebruikerService;
+
+    public void registreer( String voornaam, String achternaam, String email, String wachtwoord, String rijksregisternummer, String geboortedatum){
+        newGebruiker.setActief(false);
+        newGebruiker.setVoornaam(voornaam);
+        newGebruiker.setAchternaam(achternaam);
+        newGebruiker.setEmail(email);
+        newGebruiker.setWachtwoord(wachtwoord);
+        newGebruiker.setRijksregisternummer(rijksregisternummer);
+        newGebruiker.setGeboortedatum(new Date());
+        gebruikerService.insert(newGebruiker);
+    }
+
 
 }
--- src/main/java/be/thomasmore/travelmore/repository/GebruikerRepository.java
+++ src/main/java/be/thomasmore/travelmore/repository/GebruikerRepository.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+
 public class GebruikerRepository {
     @PersistenceContext(unitName = "travelMorePU")
     private EntityManager entityManager;
