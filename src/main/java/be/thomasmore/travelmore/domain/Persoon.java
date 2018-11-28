4e7418105c62215fa6da6b1f2299b775220c24ec
Robbert Van Hove <robbertvanhove1@gmail.com>
2018-11-28 15:16:16 +0100

    Update Persoon.java
    Removed confirmationtoken

--- src/main/java/be/thomasmore/travelmore/domain/Persoon.java
+++ src/main/java/be/thomasmore/travelmore/domain/Persoon.java
@@ -39,8 +39,6 @@ public class Persoon {
     @Size(max = 255)
     private String wachtwoord;
 
-    @Column(name = "ConfirmationToken")
-    private String ConfirmationToken;
 
     public Persoon() {
     }
@@ -85,11 +83,5 @@ public class Persoon {
         this.wachtwoord = wachtwoord;
     }
 
-    public String getConfirmationToken() {
-        return ConfirmationToken;
-    }
-
-    public void setConfirmationToken(String confirmationToken) {
-        ConfirmationToken = confirmationToken;
-    }
+    
 }
