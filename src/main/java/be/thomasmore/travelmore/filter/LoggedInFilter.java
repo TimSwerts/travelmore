3eb5f478c7271af8fc9fc54c85ae8adabfad42d3
robbert van hove <robbertvanhove1@gmail.com>
2018-12-02 13:52:50 +0100

    authguards af

---
+++ src/main/java/be/thomasmore/travelmore/filter/LoggedInFilter.java
@@ -0,0 +1,36 @@
+package be.thomasmore.travelmore.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/")
+public class LoggedInFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig config) throws ServletException{
+
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException{
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpSession session = request.getSession(false);
+
+        if(session != null && session.getAttribute("Gebruiker") != null){
+            response.sendRedirect(request.getContextPath() + "/index.xhtml");
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void destroy(){
+
+    }
+}
